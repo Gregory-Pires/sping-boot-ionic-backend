091232b46d51a08d43700cc25b1436d6f7d70f3b
Gregory Blene Alves Pires <blene.pires@gmail.com>
2019-08-05 18:30:36 -0300

    Estado e cidade

---
+++ src/main/java/com/blene/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.blene.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blene.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+}
---
+++ src/main/java/com/blene/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.blene.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blene.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+	
+}
