64c4b167df65078622c3c7d8e95b6a42206c8548
Gregory Blene Alves Pires <blene.pires@gmail.com>
2019-08-06 16:28:27 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/blene/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.blene.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blene.cursomc.domain.Cliente;
+import com.blene.cursomc.repositories.ClienteRepository;
+import com.blene.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
