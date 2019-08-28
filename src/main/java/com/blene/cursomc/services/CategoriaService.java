f658f14a7580b064e692a90f71c10e96d459832b
Gregory Blene Alves Pires <blene.pires@gmail.com>
2019-08-27 21:10:32 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/blene/cursomc/services/CategoriaService.java
+++ src/main/java/com/blene/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.blene.cursomc.domain.Categoria;
+import com.blene.cursomc.dto.CategoriaDTO;
 import com.blene.cursomc.repositories.CategoriaRepository;
 import com.blene.cursomc.services.exceptions.DataIntegrityException;
 import com.blene.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 
 }
