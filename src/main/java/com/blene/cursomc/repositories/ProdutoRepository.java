095c9ea56dfc9b557cf5dcf27fb13c21a3107ed1
Gregory Blene Alves Pires <blene.pires@gmail.com>
2019-09-09 19:25:47 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/blene/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/blene/cursomc/repositories/ProdutoRepository.java
@@ -1,12 +1,21 @@
 package com.blene.cursomc.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.blene.cursomc.domain.Categoria;
 import com.blene.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
-	
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeCntainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
