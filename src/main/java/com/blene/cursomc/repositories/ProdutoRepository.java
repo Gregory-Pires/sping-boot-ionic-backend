e98df6bbc8534100ad7926e84ed2e134ed572cf5
Gregory Blene Alves Pires <blene.pires@gmail.com>
2019-09-10 19:42:39 -0300

    Subtotal e total do pedido

--- src/main/java/com/blene/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/blene/cursomc/repositories/ProdutoRepository.java
@@ -17,5 +17,5 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	@Transactional(readOnly=true)
-	Page<Produto> findDistinctByNomeCntainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
